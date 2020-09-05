dc407184d3e16f460ee47207cf718fdeeb72caa9
Constantin Radulescu <seaman105@yahoo.com>
2020-09-05 20:05:31 +0300

    Fix

--- scripts/kubernets-curl.sh
+++ scripts/kubernets-curl.sh
@@ -11,7 +11,7 @@
 #############################################################################################################################################
 
 
-#Create pem files
+
 SERVER=$(grep server "$HOME"/.kube/config | awk '{print $2}')
 CERT_PATH="$HOME/kube-cert"
 
@@ -21,6 +21,7 @@ if [[ ! -d $CERT_PATH ]]
   CLIENT=$(grep client-cert "$HOME"/.kube/config |cut -d" " -f 6)
   KEY=$(grep client-key-data "$HOME"/.kube/config |cut -d " " -f 6)
   AUTH=$(grep certificate-authority-data "$HOME"/.kube/config |cut -d " " -f 6)
+  #Create pem files
   mkdir "$CERT_PATH"
   echo "$CLIENT" | base64 -d - > "$CERT_PATH/client.pem"
   echo "$KEY" | base64 -d - > "$CERT_PATH/client-key.pem"
